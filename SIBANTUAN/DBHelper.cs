c4e3a2c80641b54cd09249fafdde4915482d158b
radaarr <rafifdaffa0336@gmail.com>
2026-05-11 11:48:22 +0700

    Add project files.

---
+++ SIBANTUAN/DBHelper.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data.SqlClient;
+
+public class DBHelper
+{
+    // Ganti sesuai settingan MySQL masing-masing
+    private static string connectionString =
+        "Server=localhost;Database=sibantuan;Uid=root;Pwd=password_kamu;";
+
+    public static MySqlConnection GetConnection()
+    {
+        return new MySqlConnection(connectionString);
+    }
+}
